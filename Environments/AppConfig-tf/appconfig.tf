c313b96a9972608c88ee4c86bc9dea11c3f1249e
Jin Lee <jinle@microsoft.com>
2025-01-18 10:13:53 -0600

    fixing tf deploy

---
+++ Environments/AppConfig-tf/appconfig.tf
@@ -0,0 +1,34 @@
+
+resource "azurerm_app_configuration" "configStore" {
+  name                = var.name
+  location            = var.ade_location
+  resource_group_name = var.resource_group_name
+  sku = "standard"
+  
+  tags = local.tags
+}
+
+
+resource "azurerm_app_configuration_key" "configStoreKey" {
+  for_each = { for kv in var.keyValues : kv.key => kv }
+
+  configuration_store_id = azurerm_app_configuration.configStore.id
+  key                    = each.value.key
+  value                  = each.value.value
+}
+
+output "appConfigEndpoint" {
+  value = azurerm_app_configuration.configStore.endpoint
+}
+
+output "appConfigId" {
+  value = azurerm_app_configuration.configStore.id
+}
+
+output "appConfigName" {
+  value = azurerm_app_configuration.configStore.name
+}
+
+output "appConfigLocation" {
+  value = azurerm_app_configuration.configStore.location
+}
--- Environments/AppConfig-tf/main.tf
+++ Environments/AppConfig-tf/main.tf
@@ -1,3 +1,9 @@
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.ade_subscription
+}
+
 locals {
   tags = {
     environment = var.env_name
@@ -5,36 +11,4 @@ locals {
     ade_environment_type = var.ade_environment_type
     ade_subscription = var.ade_subscription
   }
-}
-
-resource "azurerm_app_configuration" "configStore" {
-  name                = var.name
-  location            = var.ade_location
-  resource_group_name = var.resource_group_name
-  sku = "standard"
-}
-
-
-resource "azurerm_app_configuration_key" "configStoreKey" {
-  for_each = { for kv in var.keyValues : kv.key => kv }
-
-  configuration_store_id = azurerm_app_configuration.configStore.id
-  key                    = each.value.key
-  value                  = each.value.value
-}
-
-output "appConfigEndpoint" {
-  value = azurerm_app_configuration.configStore.endpoint
-}
-
-output "appConfigId" {
-  value = azurerm_app_configuration.configStore.id
-}
-
-output "appConfigName" {
-  value = azurerm_app_configuration.configStore.name
-}
-
-output "appConfigLocation" {
-  value = azurerm_app_configuration.configStore.location
 }
